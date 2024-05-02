74f91258280ab02332ebb2df06be18f873548c2e
dhruvghimiray <dhruvg0231@gmail.com>
2024-05-02 16:54:09 +0530

    side fade in navbar

---
+++ script.js
@@ -0,0 +1,30 @@
+const tl = gsap.timeline();
+
+const open = document.querySelector(".ri-menu-line");
+const close = document.querySelector(".ri-close-line");
+
+tl.to("#nav-open", {
+  right: 0,
+  duration: 0.5,
+});
+
+tl.from("ul li", {
+  x: 100,
+  duration: 0.6,
+  stagger: 0.2,
+  opacity: 0,
+});
+
+tl.from("#nav-open i", {
+  opacity: 0,
+});
+
+tl.pause();
+
+open.addEventListener("click", () => {
+  tl.play();
+});
+
+close.addEventListener("click", () => {
+  tl.reverse();
+});
